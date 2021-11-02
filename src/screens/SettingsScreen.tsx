7653c3a21c78eb0b3753f00ea2495d2fb45b3f54
Miguel Veloso <obeloke@hotmail.com>
2021-11-02 00:40:28 -0300

    Creación Drawer Menu

---
+++ src/screens/SettingsScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+// import { styles } from '../theme/appTheme';
+
+export const SettingsScreen = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <Text>
+                Settings Screen
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
